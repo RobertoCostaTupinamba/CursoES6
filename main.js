c3acdbe855e3697c97983e11a3750550e4c394d1
Roberto Costa Tupinambá <robertotupinamba@gmail.com>
2019-11-19 21:44:34 -0300

    Classes

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-class Matematica {
-    metodo(){
-        
+
+class List {
+    constructor (){
+        this.data = [];
     }
 
-    outro(){
-
+    add(data){
+        this.data.push(data);
     }
+}
+
+
+class TodoList extends List {
+  constructor(){
+    this.todos = [];
+  }
+  
+  addTodo(){
+      this.todos.push('Novo tudo');
+      console.log(this.todos);
+  }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function () {
+    MinhaLista.addTodo();
 }
