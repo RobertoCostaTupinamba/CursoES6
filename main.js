061589e023dce2b8c7a6bb5c3008f4c56e259b4e
Roberto Costa Tupinambá <robertotupinamba@gmail.com>
2019-11-19 22:43:29 -0300

    Metodo Statico

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
 
-class List {
-    constructor (){
-        this.data = [];
-    }
-
-    add(data){
-        this.data.push(data);
-        console.log(this.data);
-        
+class Matematica{
+    static soma(a , b){
+        return a + b;
     }
 }
 
+console.log(Matematica.soma(1,2));
 
-class TodoList extends List {
-    constructor(){
-        super();
 
-        this.usuario = 'Roberto';
-    }
+//------------------Herançaa
+// class List {
+//     constructor (){
+//         this.data = [];
+//     }
+
+//     add(data){
+//         this.data.push(data);
+//         console.log(this.data);
+        
+//     }
+// }
+
+
+// class TodoList extends List {
+//     constructor(){
+//         super();
+
+//         this.usuario = 'Roberto';
+//     }
     
-    mostrarUsuario(){
-        console.log(this.usuario);
+//     mostrarUsuario(){
+//         console.log(this.usuario);
         
-    }
-}
+//     }
+// }
 
-const MinhaLista = new TodoList();
+// const MinhaLista = new TodoList();
 
-document.getElementById('novotodo').onclick = function () {
-    MinhaLista.add('NovoTodo');
-}
+// document.getElementById('novotodo').onclick = function () {
+//     MinhaLista.add('NovoTodo');
+// }
 
-MinhaLista.mostrarUsuario();
+// MinhaLista.mostrarUsuario();
